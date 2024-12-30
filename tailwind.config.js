dbad7809f3e6c4f8a8c469fa85ff0fd835f83b78
JGS <8240026+jgsong@users.noreply.github.com>
2024-12-30 03:03:31 -0800

    Updating tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './pages/**/*.{html,js}',
     './index.html',
-    "./**/*.{html,js}",
   ],
   theme: {
+    // colors: {
+    //   //Overwrite theme colors
+    //   transparent: 'transparent',
+    //   current: 'currentColor',
+    //   black: colors.black,
+    //   white: colors.white,
+    //   gray: colors.slate,
+    //   green: colors.emerald,
+    //   purple: colors.violet,
+    //   yellow: colors.amber,
+    //   pink: colors.fuchsia,  
+    //   'blue': '#1fb6ff',
+    //   'purple': '#7e5bef',
+    //   'pink': '#ff49db',
+    //   'orange': '#ff7849',
+    //   'green': '#13ce66',
+    //   'yellow': '#ffc82c',
+    //   'gray-dark': '#273444',
+    //   'gray': '#8492a6',
+    //   'gray-light': '#d3dce6',
+    // },
+    
+    // fontFamily:{
+    //   //Overwrite default font families (sans, serif, display, monospace) 
+    //   //Tip: Use double-quotes ("") for font names that have spaces
+    //   //TODO: Figure out how to import google fonts
+    //   'sans': ['Inter, sans-serif'], 
+    //   'serif': []
+    //   'display': ['"Work Sans", Helvetica, Arial, sans-serif']
+    // },
     extend: {},
   },
   plugins: [],
