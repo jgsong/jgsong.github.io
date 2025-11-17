784fa92702ea44f78c983703ccefc7f2544b2e71
JGS <8240026+jgsong@users.noreply.github.com>
2025-11-16 21:32:07 -0800

    updated edu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    './work/**/*.{html,js}',
-    './index.html',
-  ],
+  content: ["./work/**/*.{html,js}", "./index.html"],
   theme: {
     // colors: {
     //   //Overwrite theme colors
     //   transparent: 'transparent',
     //   current: 'currentColor',
 
-    //   //These are examples of aliasing  
+    //   //These are examples of aliasing
     //   black: colors.black,
     //   white: colors.white,
     //   gray: colors.slate,
     //   green: colors.emerald,
     //   purple: colors.violet,
     //   yellow: colors.amber,
-    //   pink: colors.fuchsia,  
+    //   pink: colors.fuchsia,
 
     //   //These are examples of custom colors
     //   'blue': '#1fb6ff',
@@ -35,33 +32,30 @@ module.exports = {
     // },
     fontFamily: {
       //Overwrite default font families - sans, serif, mono, display (tried, but doesn't seem to work)
-      //Default themes included as backups 
+      //Default themes included as backups
       //Tip: Use double-quotes ("") for font names that have spaces
-      'sans': ['"Libre Franklin"', ...defaultTheme.fontFamily.sans],
-      'serif': ['"DM Serif Text"', ...defaultTheme.fontFamily.serif],
+      sans: ['"Libre Franklin"', ...defaultTheme.fontFamily.sans],
+      serif: ['"DM Serif Text"', ...defaultTheme.fontFamily.serif],
       //'mono': ['addMonoFontHere', ...defaultTheme.fontFamily.mono],
     },
     extend: {
       fontFamily: {
         //Utility suffixes (eg. font-ptsans)
-        'display': '"DM Serif Display"',
-        'dmserif': '"DM Serif Text"',
-        'dmserifdisplay': '"DM Serif Display"',
+        display: '"DM Serif Display"',
+        dmserif: '"DM Serif Text"',
+        dmserifdisplay: '"DM Serif Display"',
       },
       typography: {
         DEFAULT: {
           css: {
-            //maxWidth: '100ch', //default is 65, recommended is 50-75
-          }
-        }
+            maxWidth: "100ch", //default is 65, recommended is 50-75
+          },
+        },
       },
       aspectRatio: {
-        '3/2': '3 / 2',
+        "3/2": "3 / 2",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
-}
-
+  plugins: [require("@tailwindcss/typography")],
+};
